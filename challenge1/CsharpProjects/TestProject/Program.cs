0cb19c5f04687c689dc1b69ea9e32970056047b9
DyllanKintner <Dyllankintner2@gmail.com>
2025-04-04 10:34:46 -0700

    c#

--- challenge1/CsharpProjects/TestProject/Program.cs
+++ challenge1/CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] IDs = {"B123", "C234", "A345", "C15", "B177", "G3003" ,"C235", "B179"}; 
+foreach (string ID in IDs) // Loops through each ID in the array
+{
+    if (ID.StartsWith("B")) // Checks if the current ID starts with "B"
+    {
+        Console.WriteLine(ID); // Prints the ID only if it starts with "B"
+    }
+}
