2cbd8db051cd92a08587e8933bb093d53e5c7649
DyllanKintner <Dyllankintner2@gmail.com>
2025-04-04 08:26:35 -0700

    c#

--- lernArrayInt/CsharpProjects/intarray/Program.cs
+++ lernArrayInt/CsharpProjects/intarray/Program.cs
@@ -1,12 +1,15 @@
-﻿int[] inventory = {200, 450, 700, 175, 250};
-int sum = 0;
+﻿int[] inventory = {200, 450, 700, 175, 250};// this is an array 
+int sum = 0; // Initializes sum to store the total number of items 
 
-int bin = 0;
+int bin = 0; //Initializes bin to track the bin number.
+ 
 
-foreach (int items in inventory)
+foreach (int items in inventory)// loops though each ietm in the inventory array
 {
-    sum += items;
-    bin++;
-    Console.WriteLine($"Bin {bin} = {items} items (running total: {sum})");
+    sum += items; // adds 0 + 200 then 200 + 450 ect
+    bin++; //adds 1 to bin so 0 +1 the next loop 1+1 ect 
+    // Prints the bin number, item count, and running total of items.
+    Console.WriteLine($"Bin {bin} = {items} items (running total: {sum})"); 
 }
-Console.WriteLine($"We have {sum} items in inventory.");
+// Prints the total number of items in all bins combined.
+Console.WriteLine($"We have {sum} items in inventory."); 
