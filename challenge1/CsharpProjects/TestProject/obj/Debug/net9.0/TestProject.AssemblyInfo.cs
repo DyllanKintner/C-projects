3d3f3b1a36a307e4f563593770fe319c920952f0
DyllanKintner <Dyllankintner2@gmail.com>
2025-04-04 19:01:00 -0700

    c#

--- challenge1/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ challenge1/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cbd8db051cd92a08587e8933bb093d53e5c7649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb19c5f04687c689dc1b69ea9e32970056047b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
