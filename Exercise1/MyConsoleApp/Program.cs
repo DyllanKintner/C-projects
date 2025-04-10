d4b0df5ca1bdda6803d96f597cb9f4722c7f757e
DyllanKintner <Dyllankintner2@gmail.com>
2025-04-10 10:34:42 -0700

    c#

---
+++ Exercise1/MyConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+Console.WriteLine(permission.Contains("Admin") && level > 55 ? "Welcome, Super Admin user." : "Welcome, user.");
+Console.WriteLine(permission.Contains("Admin") && level <= 55 ? "Welcome, Admin user." : "Welcome, user.");
+Console.WriteLine(permission.Contains("manager") && level > 20 ? "contact and admin for access" : "Welcome, user.");
+Console.WriteLine(
+    !permission.Contains("Admin") && !permission.Contains("Manager")
+    ? "You do not have sufficient privileges."
+    : "Welcome, user."
+);
+
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
---
+++ foreachprj/Program.cs
@@ -0,0 +1,35 @@
+﻿//Console.WriteLine("a" == "a");
+//Console.WriteLine("a" == "A");
+//Console.WriteLine(1 == 2);
+
+//string myValue = "a";
+//Console.WriteLine(myValue != "a");
+
+//string value1 = " a";
+//string value2 = "A ";
+//Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+//string pangram = "The quick brown fox jumps over the lazy dog";
+//Console.WriteLine(pangram.Contains("fox"));
+//Console.WriteLine(pangram.Contains("cow"));
+
+
+// These two lines of code will create the same output
+//Console.WriteLine(pangram.Contains("fox") == false);
+//Console.WriteLine(!pangram.Contains("fox"));
+
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(!pangram.Contains("fox"));
+//Console.WriteLine(!pangram.Contains("cow"));
+
+//int a = 7;
+//int b = 6;
+//Console.WriteLine(a != b); // output: True
+//string s1 = "Hello";
+//string s2 = "Hello";
+//Console.WriteLine(s1 != s2); // output: False
+
+int saleAmount = 1001;
+int discount = saleAmount > 1000  ? 100 : 50;
+//Console.WriteLine($"discount {discount}"); 
+Console.WriteLine($"discount {(saleAmount > 1000 ? 100 : 50)}"); // output: discount 100
