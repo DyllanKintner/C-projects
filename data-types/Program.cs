4bb25768e4fc6de68800455c6aacbfcef498672d
DyllanKintner <Dyllankintner2@gmail.com>
2025-05-06 09:07:08 -0700

    C#

--- data-types/Program.cs
+++ data-types/Program.cs
@@ -23,7 +23,7 @@ int[] data = new int[3];
 
 string shortenedString = "Hello World!";
 Console.WriteLine(shortenedString);
-*/
+
 
 int[] ref_A= new int[1];
 ref_A[0] = 2;
@@ -33,3 +33,50 @@ ref_B[0] = 5;
 Console.WriteLine("--Reference Types--");
 Console.WriteLine($"ref_A[0]: {ref_A[0]}");
 Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+
+
+
+int frist =2;
+string secound = "4";
+int result = frist + secound; // This will cause a compile-time error because you cannot add an int and a string directly.
+Console.WriteLine(result);
+
+int first = 2;
+string second = "4";
+string result = first + second; 
+Console.WriteLine(result);
+
+
+int myInt = 3;
+Console.WriteLine($"int : {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal : {myDecimal}");
+
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
+
+int frist = 2;
+int secound = 4;   
+string message = frist.ToString() + secound.ToString();  // the ToString method converts the int to a string
+//57 
+
+string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second); // the Parse method converts the string to an int
+Console.WriteLine(sum);
+*/
+
+string value = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value) + Convert.ToInt32(value2); // the Convert.ToInt32 method converts the string to an int
+Console.WriteLine(result); // 12
+string value1s = "5";
+string value2s = "7";
+int results = Convert.ToInt32(value1s) * Convert.ToInt32(value2s);
+Console.WriteLine(results);// 35
+
+// Convert.Toint32 rounds to the nearest whole number but (int)1.5m will just remove the decimal part
---
+++ tryprase/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+string name = "Bob";
+Console.WriteLine(int.Parse(name)); // This will throw a System.FormatException because "Bob" cannot be converted to an integer.
+*/
+
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+   Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
+                 
